2884ef8f315b0da8004532a930300fd14d74f500
chenxuting <chenxuting@corp.netease.com>
2022-11-28 16:17:23 +0800

    feat: add neGroupCall

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,67 @@
 <template>
   <div>
-  <CallViewProvider v-if="!!neCallConfig.nim" :neCallConfig="neCallConfig"  ref="callViewProviderRef"/>
-  <div :class="$style.container">
-    <Provider v-if="!!nimKitCore" :nimKitCore="nimKitCore" :initOptions="initOptions" >
-      <div :class="$style.header">
-        <div :class="$style.search">
-          <SearchContainer />
-        </div>
-        <div :class="$style.add">
-          <AddContainer />
-        </div>
-      </div>
-      <div :class="$style.content">
-        <div :class="$style.left">
-          <div :class="$style['avatar-icon']">
-            <MyAvatarContainer />
+    <!-- 单呼组件 -->
+    <CallViewProvider v-if="!!neCallConfig.nim" :neCallConfig="neCallConfig" ref="callViewProviderRef" />
+    <!-- 群呼组件 -->
+    <GroupCallViewProvider v-if="!!neCallConfig.nim" :neGroupCallConfig="neCallConfig" ref="groupCallViewProviderRef" />
+    <div :class="$style.container">
+      <!-- IMUIKIT 相关内容 -->
+      <Provider v-if="!!nimKitCore" :nimKitCore="nimKitCore" :initOptions="initOptions">
+        <div :class="$style.header">
+          <div :class="$style.search">
+            <SearchContainer />
           </div>
-          <div :class="{ [$style['chat-icon']]: true, [$style.active]: model === 'chat' }"
-            @click="() => model = 'chat'">
-            <SmileFilled />
-            <div :class="$style['icon-label']">
-              会话
+          <div :class="$style.add">
+            <AddContainer />
+          </div>
+        </div>
+        <div :class="$style.content">
+          <div :class="$style.left">
+            <div :class="$style['avatar-icon']">
+              <MyAvatarContainer />
+            </div>
+            <div :class="{ [$style['chat-icon']]: true, [$style.active]: model === 'chat' }"
+              @click="() => model = 'chat'">
+              <SmileFilled />
+              <div :class="$style['icon-label']">
+                会话
+              </div>
+            </div>
+            <div :class="{ [$style['contact-icon']]: true, [$style.active]: model === 'contact' }"
+              @click="() => model = 'contact'">
+              <MobileFilled />
+              <div :class="$style['icon-label']">
+                通讯录
+              </div>
+            </div>
+            <div :class="$style['logout-icon']">
+              <LoginOutlined />
+              <div :class="$style['icon-label']">
+                退出
+              </div>
             </div>
           </div>
-          <div :class="{ [$style['contact-icon']]: true, [$style.active]: model === 'contact' }"
-            @click="() => model = 'contact'">
-            <MobileFilled />
-            <div :class="$style['icon-label']">
-              通讯录
+          <div :class="$style.right" v-if="model === 'chat'">
+            <div :class="$style['right-list']">
+              <ConversationContainer />
+            </div>
+            <div :class="$style['right-content']">
+              <ChatContainer :actions="chatInputActions" />
             </div>
           </div>
-          <div :class="$style['logout-icon']" >
-            <LoginOutlined />
-            <div :class="$style['icon-label']">
-              退出
+          <div :class="$style.right" v-if="model === 'contact'">
+            <div :class="$style['right-list']">
+              <ContactListContainer />
+            </div>
+            <div :class="$style['right-content']">
+              <ContactInfoContainer :afterSendMsgClick="() => model = 'chat'"
+                :onGroupItemClick="() => model = 'chat'" />
             </div>
           </div>
         </div>
-        <div :class="$style.right" v-if="model === 'chat'">
-          <div :class="$style['right-list']">
-            <ConversationContainer />
-          </div>
-          <div :class="$style['right-content']">
-            <ChatContainer :actions="chatInputActions" />
-          </div>
-        </div>
-        <div :class="$style.right" v-if="model === 'contact'">
-          <div :class="$style['right-list']">
-            <ContactListContainer />
-          </div>
-          <div :class="$style['right-content']">
-            <ContactInfoContainer :afterSendMsgClick="() => model = 'chat'" :onGroupItemClick="() => model = 'chat'" />
-          </div>
-        </div>
-      </div>
-    </Provider>
+      </Provider>
+    </div>
   </div>
-</div>
 </template>
 <script>
 // import Vue from 'vue'
@@ -71,7 +76,7 @@ import {
   ContactInfoContainer, // 通讯录——通讯录详情组件，包含好友列表、群组列表以及黑名单列表
   MyAvatarContainer, // 用户资料组件
 } from '@xkit-yx/im-kit-ui'
-import { CallViewProvider } from "@xkit-yx/call-kit-react-ui";
+import { CallViewProvider, GroupCallViewProvider } from "@xkit-yx/call-kit-react-ui";
 import Button from 'antd/lib/button'
 import MobileFilled from '@ant-design/icons/MobileFilled';
 import SmileFilled from '@ant-design/icons/SmileFilled';
@@ -79,7 +84,7 @@ import LoginOutlined from '@ant-design/icons/LoginOutlined';
 import AudioOutlined from '@ant-design/icons/AudioOutlined';
 import VideoCameraOutlined from '@ant-design/icons/VideoCameraOutlined';
 import { NimKitCoreFactory } from '@xkit-yx/core-kit'
-import {compile} from 'jsx-web-compiler'
+import { compile } from 'jsx-web-compiler'
 import React from 'react'
 // 引入样式
 import '@xkit-yx/im-kit-ui/es/style'
@@ -93,6 +98,7 @@ export default {
     SmileFilled: applyReactInVue(SmileFilled),
     LoginOutlined: applyReactInVue(LoginOutlined),
     CallViewProvider: applyReactInVue(CallViewProvider),
+    GroupCallViewProvider: applyReactInVue(GroupCallViewProvider),
     Provider: applyReactInVue(Provider),
     ConversationContainer: applyReactInVue(ConversationContainer),
     ChatContainer: applyReactInVue(ChatContainer),
@@ -102,9 +108,9 @@ export default {
     ContactInfoContainer: applyReactInVue(ContactInfoContainer),
     MyAvatarContainer: applyReactInVue(MyAvatarContainer),
   },
-  data: function() {
+  data: function () {
     return {
-       // imuikit配置
+      // imuikit配置
       initOptions: {
         appkey: '', //传入您的App Key
         account: '', // 传入您的云信IM账号
@@ -119,12 +125,11 @@ export default {
         debug: true
       },
       chatInputActions: [
-      { 
+        {
           action: 'audio',
           visible: true,
           render: (data) => {
             const call = () => {
-              console.log('accId', data.to)
               this.$refs.callViewProviderRef.reactRef.call(
                 {
                   accId: data.to,
@@ -132,42 +137,55 @@ export default {
                 }
               )
             }
+            // 群呼功能不区分语音跟视频，所以隐藏语音按钮
+            if (data.scene === 'team') {
+              return null
+            }
             // 方案1
             const icon = React.createElement(AudioOutlined, { style: { fontSize: '20px', color: '#656a72' } })
             return React.createElement(Button, { type: 'default', onClick: call }, icon)
           }
         },
-        { 
+        {
           action: 'video',
           visible: true,
           render: (data) => {
             const call = () => {
-              this.$refs.callViewProviderRef.reactRef.call(
-                {
-                  accId: data.to,
-                  callType: '2',
-                }
-              )
+              if (data.scene === 'team') {
+                // demo中全量拨打，实际使用中，可以根据业务需求，写个弹窗，在弹窗中勾选对应呼叫的成员后，再调用groupCall方法
+                this.nimKitCore.getTeamMembers({ teamId: data.to }).then((res) => {
+                  // 通过接口获取群成员列表，并过滤自己
+                  const calleeList = res.map((item) => item.account).filter((item) => item !== this.neCallConfig.currentUserInfo.accId)
+                  this.$refs.groupCallViewProviderRef.reactRef.groupCall({ calleeList })
+                })
+              } else if (data.scene === 'p2p') {
+                this.$refs.callViewProviderRef.reactRef.call(
+                  {
+                    accId: data.to,
+                    callType: '2',
+                  }
+                )
+              }
             }
             // 方案2
             return compile(`(
               (Button, VideoCameraOutlined, call) => {
               return <Button type="default" onClick={call}><VideoCameraOutlined style={{ fontSize: '20px', color: '#656a72' }} /></Button>
             }
-            )(...arguments)`, null,  Button, VideoCameraOutlined, call )
+            )(...arguments)`, null, Button, VideoCameraOutlined, call)
             // const icon = React.createElement(VideoCameraOutlined, { style: { fontSize: '20px', color: '#656a72' } })
             // return React.createElement(Button, { type: 'default', onClick: call }, icon)
           }
         },
-        { 
+        {
           action: 'emoji',
           visible: true,
         },
-        { 
+        {
           action: 'sendImg',
           visible: true,
         },
-        { 
+        {
           action: 'sendFile',
           visible: true,
         }
@@ -181,10 +199,19 @@ export default {
   },
   mounted: function () {
     const NIM = NimKitCoreFactory(1)
-    const nimKitCore = new NIM({ initOptions: this.initOptions })
+    const nimKitCore = new NIM({
+      initOptions: {
+        ...this.initOptions,
+        // 群呼在这接收邀请
+        oncustomsysmsg: (msg) => {
+          if (msg.type === 'custom') {
+            this.$refs.groupCallViewProviderRef.reactRef.onMsg(msg)
+          }
+        },
+      }
+    })
     this.nimKitCore = nimKitCore
-    nimKitCore.on('logined', (res) => {
-      console.log('-------logined--------', res)
+    nimKitCore.on('logined', () => {
       this.neCallConfig.nim = nimKitCore.nim
     })
   }
